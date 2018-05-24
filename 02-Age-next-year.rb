3fb5be1a22a59e325a301e2913d4bbf18e854590
Martins Giovanni <giovannimartins2000@gmail.com>
2018-05-24 17:16:47 -0300

    adding exercise 02

---
+++ 02-Age-next-year.rb
@@ -0,0 +1,14 @@
+def age_next_year(age)
+  actual_age = age + 1
+  "Next year I'll be '#{actual_age}' years old."
+end
+
+# age_next_year(19) should return:
+# Next year I'll be 20 years old.
+puts age_next_year(19)
+
+# age_next_year(30) should return:
+# Next year I'll be 31 years old.
+puts age_next_year(30)
+
+puts age_next_year(25)
